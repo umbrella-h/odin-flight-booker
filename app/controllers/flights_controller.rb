51a75a33ab69cd7052ddbcceaee9c082666bf293
umbrella-h <umbrella.hsiao@gmail.com>
2020-11-22 15:39:32 +0800

    a temporary none error verson

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,10 +5,10 @@ class FlightsController < ApplicationController
 	end
 	
 	def create
-		@flight = Flight.new(flightt_params)
+		@flight = Flight.new(flight_params)
 	end
 	private
 		def flight_params
-	      params.require(:flight).permit(:dep_time, :duration)
+	      params.require(:flight).permit(:dep_airport_id, :arr_airport_id, :dep_time, :duration)
 		end
 end
