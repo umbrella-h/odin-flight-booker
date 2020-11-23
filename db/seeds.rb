4a4ee9519c64051d1b3bea2e935fd1cdca7970fc
umbrella-h <umbrella.hsiao@gmail.com>
2020-11-23 10:24:09 +0800

    use create! in seeds.rb, and find now the db need bookings for saving flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@ ActiveRecord::Base.connection.tables.each do |table|
 end
 
 
-	airports = Airport.create([{ code: 'SFO' }, { code: 'NYC' }])
-	flights = Flight.create([
+	airports = Airport.create!([{ code: 'SFO' }, { code: 'NYC' }])
+	flights = Flight.create!([
 	{dep_airport_id: airports.first.id, arr_airport_id: airports.last.id, dep_time: "2020-12-01 08:00:00", duration: 6}, 
 	{dep_airport_id: airports.first.id, arr_airport_id: airports.last.id, dep_time: "2020-12-01 09:00:00", duration: 6}, 
 	{dep_airport_id: airports.first.id, arr_airport_id: airports.last.id, dep_time: "2020-12-01 19:40:00", duration: 7}, 
@@ -17,6 +17,7 @@ end
 	{dep_airport_id: airports.last.id, arr_airport_id: airports.first.id, dep_time: "2020-12-02 13:00:00", duration: 8}, 
 	{dep_airport_id: airports.last.id, arr_airport_id: airports.first.id, dep_time: "2020-12-02 17:20:00", duration: 9}	
 	])
+
 
 #DateTime.civil_from_format :local, 2020, 12, 1
 
