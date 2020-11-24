0a80ab51cac1fe7738ae7b6477dfbae93918b830
umbrella-h <umbrella.hsiao@gmail.com>
2020-11-24 20:47:10 +0800

    Searching feature completely done (leave the radio_button vertical-alignment problem

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,14 +2,24 @@ class FlightsController < ApplicationController
 	def index
 		@airports = Airport.all
 		@flights = Flight.all
+		@result_flights = Flight.where(dep_airport_id: "---")
+
+		if params.has_key?(:dep_time_yr) && params.has_key?(:dep_time_mon) && params.has_key?(:dep_time_day)
+			@selected_date = DateTime.civil(params[:dep_time_yr].to_i, params[:dep_time_mon].to_i, params[:dep_time_day].to_i)
+			@result_flights = Flight.where(dep_airport_id: params[:dep_airport_id], arr_airport_id: params[:arr_airport_id]).where(dep_time: @selected_date.all_day)
+			#@result_flights = Flight.where( dep_time: @selected_date.all_day)
+			
+		end
 		
-		#@selected_date = Date.civil(params[:dep_time_yr].to_i, params[:dep_time_mon].to_i, params[:dep_time_day].to_i)
-		@result_flights = Flight.where(dep_airport_id: params[:dep_airport_id], arr_airport_id: params[:arr_airport_id], dep_time: "2020-12-02 13:00:00")#@selected_date.all_day)
+		#('dep_time < ? AND dep_time >= ?', @selected_date.beginning_of_day)
+	
+		console		
 	end
 	
 	def create
 		@flight = Flight.new(flight_params)
 	end
+	
 	private
 		def flight_params
 	      params.require(:flight).permit(:dep_airport_id, :arr_airport_id, :dep_time, :duration)
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,10 @@ end
 	airports = Airport.create!([{ code: 'SFO' }, { code: 'NYC' }])
 	flights = Flight.create!([
 	{dep_airport_id: airports.first.id, arr_airport_id: airports.last.id, dep_time: "2020-12-01 08:00:00", duration: 6}, 
-	{dep_airport_id: airports.first.id, arr_airport_id: airports.last.id, dep_time: "2020-12-01 09:00:00", duration: 6}, 
+	{dep_airport_id: airports.first.id, arr_airport_id: airports.last.id, dep_time: "2020-12-02 09:00:00", duration: 6}, 
 	{dep_airport_id: airports.first.id, arr_airport_id: airports.last.id, dep_time: "2020-12-01 19:40:00", duration: 7}, 
 	{dep_airport_id: airports.last.id, arr_airport_id: airports.first.id, dep_time: "2020-12-02 10:00:00", duration: 8}, 
-	{dep_airport_id: airports.last.id, arr_airport_id: airports.first.id, dep_time: "2020-12-02 13:00:00", duration: 8}, 
+	{dep_airport_id: airports.last.id, arr_airport_id: airports.first.id, dep_time: "2020-12-01 13:00:00", duration: 8}, 
 	{dep_airport_id: airports.last.id, arr_airport_id: airports.first.id, dep_time: "2020-12-02 13:00:00", duration: 9}	
 	])
 
