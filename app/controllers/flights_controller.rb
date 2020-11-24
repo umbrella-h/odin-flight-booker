b5f4024e92a3a383a6923d3a2fd6bbba15aaa5f6
umbrella-h <umbrella.hsiao@gmail.com>
2020-11-25 03:42:41 +0800

    Fix a .blank? problem for DateTime.civil() in Searching feature

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,15 +5,14 @@ class FlightsController < ApplicationController
 		@result_flights = Flight.where(dep_airport_id: "---")
 
 		if params.has_key?(:dep_time_yr) && params.has_key?(:dep_time_mon) && params.has_key?(:dep_time_day)
-			@selected_date = DateTime.civil(params[:dep_time_yr].to_i, params[:dep_time_mon].to_i, params[:dep_time_day].to_i)
-			@result_flights = Flight.where(dep_airport_id: params[:dep_airport_id], arr_airport_id: params[:arr_airport_id]).where(dep_time: @selected_date.all_day)
-			#@result_flights = Flight.where( dep_time: @selected_date.all_day)
-			
-		end
-		
-		#('dep_time < ? AND dep_time >= ?', @selected_date.beginning_of_day)
-	
-		console		
+			unless params[:dep_time_yr].blank? || params[:dep_time_mon].blank? || params[:dep_time_day].blank?
+				@selected_date = DateTime.civil(params[:dep_time_yr].to_i, params[:dep_time_mon].to_i, params[:dep_time_day].to_i)
+				@result_flights = Flight.where(dep_airport_id: params[:dep_airport_id], arr_airport_id: params[:arr_airport_id]).where(dep_time: 	@selected_date.all_day)
+				#('dep_time < ? AND dep_time >= ?', @selected_date.beginning_of_day)
+
+			end
+		end	
+		#console		
 	end
 	
 	def create
