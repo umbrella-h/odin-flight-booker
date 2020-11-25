a8dec111d3ce609d582cb4b2b125aa71cb971b5a
umbrella-h <umbrella.hsiao@gmail.com>
2020-11-25 14:13:20 +0800

    Update flights/_result.html.erb, show avialble n of seats in the searching result

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,12 @@ end
 
 	airports = Airport.create!([{ code: 'SFO' }, { code: 'NYC' }])
 	flights = Flight.create!([
-	{dep_airport_id: airports.first.id, arr_airport_id: airports.last.id, dep_time: "2020-12-01 08:00:00", duration: 6}, 
-	{dep_airport_id: airports.first.id, arr_airport_id: airports.last.id, dep_time: "2020-12-02 09:00:00", duration: 6}, 
-	{dep_airport_id: airports.first.id, arr_airport_id: airports.last.id, dep_time: "2020-12-01 19:40:00", duration: 7}, 
-	{dep_airport_id: airports.last.id, arr_airport_id: airports.first.id, dep_time: "2020-12-02 10:00:00", duration: 8}, 
-	{dep_airport_id: airports.last.id, arr_airport_id: airports.first.id, dep_time: "2020-12-01 13:00:00", duration: 8}, 
-	{dep_airport_id: airports.last.id, arr_airport_id: airports.first.id, dep_time: "2020-12-02 13:00:00", duration: 9}	
+	{dep_airport_id: airports.first.id, arr_airport_id: airports.last.id, dep_time: "2020-12-01 08:00:00", duration: 6, capacity: 100, avail: 100}, 
+	{dep_airport_id: airports.first.id, arr_airport_id: airports.last.id, dep_time: "2020-12-02 09:00:00", duration: 6, capacity: 100, avail: 100}, 
+	{dep_airport_id: airports.first.id, arr_airport_id: airports.last.id, dep_time: "2020-12-01 19:40:00", duration: 7, capacity: 110, avail: 110}, 
+	{dep_airport_id: airports.last.id, arr_airport_id: airports.first.id, dep_time: "2020-12-02 10:00:00", duration: 8, capacity: 120, avail: 120}, 
+	{dep_airport_id: airports.last.id, arr_airport_id: airports.first.id, dep_time: "2020-12-01 13:00:00", duration: 8, capacity: 125, avail: 125}, 
+	{dep_airport_id: airports.last.id, arr_airport_id: airports.first.id, dep_time: "2020-12-02 13:00:00", duration: 9, capacity: 150, avail: 150}	
 	])
 
 
