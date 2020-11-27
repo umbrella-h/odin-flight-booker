ec94a7d6329c31de7c0c428283c2be9cd982808b
umbrella-h <umbrella.hsiao@gmail.com>
2020-11-27 13:19:04 +0800

    Update passengers_controller, render current_flight in passengers/new.html.erb

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -3,10 +3,12 @@ class PassengersController < ApplicationController
 
    def new
    	@selected_flight_id = params[:selected_flight].to_i
+   	@current_flight = Flight.where(id: @selected_flight_id)
    	console
 	   #params = { user_passenger: { name: params[:name], email: params[:email]}}#, booking_attributes: { flight_id: @selected_flight_id, passenger_n: params[passenger_n] }}}
-   	@user_passenger = Passenger.create(name: params[:name], email: params[:email])#params[:user_passenger])
-   	
+	   unless params[:name].blank? || params[:email].blank? || params[:passenger_n].blank?
+   		@user_passenger = Passenger.create(name: params[:name], email: params[:email])#params[:user_passenger])
+   	end
 
    end
 	
