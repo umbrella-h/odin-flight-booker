92539be24bbbb5ff0550c2953a9ed48847302c87
umbrella-h <umbrella.hsiao@gmail.com>
2020-11-28 10:08:48 +0800

    In passengers_controller, #index, if a new booking is saved, update available seats minus @passenger_n

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -7,15 +7,28 @@ class PassengersController < ApplicationController
    	@passenger_n = params[:passenger_n].to_i
    	console
 	   params_nested = { user_passenger: { name: params[:name], email: params[:email], bookings_attributes: [{ flight_id: @selected_flight_id, passenger_n: @passenger_n}]}}
-	   unless params[:name].blank? || params[:email].blank?# || params[:passenger_n].blank?
-	   	
-   		@user_passenger = Passenger.create!(params_nested[:user_passenger]) 
-   		if @user_passenger.save
-	     		redirect_to passenger_path(@user_passenger.id)
-  			else
-    	  		render 'new'
-  			end   	  		
-   	end
+	   @same_email_p = Passenger.where(email: params[:email])
+	   
+	   if @same_email_p.blank?
+	 	  unless params[:name].blank? || params[:email].blank? || params[:passenger_n].blank?	   	
+   			@user_passenger = Passenger.create!(params_nested[:user_passenger]) 
+   			if @user_passenger.save
+   				@pre_avail = Flight.find(@selected_flight_id).avail
+   				Flight.update(@selected_flight_id, avail: @pre_avail- @passenger_n)
+	 	    		redirect_to passenger_path(@user_passenger.id)
+  				else
+    		  		render 'new'
+  				end   	  		
+   		end
+   	else
+   		if @same_email_p.bookings.where(flight_id: @selected_flight_id).blank?
+   				
+   		
+   		else
+   			
+   		
+   		end	
+   	end	
    end
 	
 	def show
@@ -34,6 +47,11 @@ class PassengersController < ApplicationController
     	  #render 'new'
     	#  redirect_to passenger_path(@passenger.id)
   		#end
+	end
+	
+	#in need??
+	def edit
+		@passenger = Passenger.find(params[:id])
 	end
 	
 	def update
