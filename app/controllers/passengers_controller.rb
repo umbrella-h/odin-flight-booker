637bdb5a53501ed26fe1d8ef188fe1aaa35655d6
umbrella-h <umbrella.hsiao@gmail.com>
2020-11-28 18:11:14 +0800

    Add flash[:notice] in passengers_controller, show.html.erb, new.html.erb

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -23,14 +23,14 @@ class PassengersController < ApplicationController
    	else
    		if @same_email_p.bookings.where(flight_id: @selected_flight_id).blank?
    			@same_email_p.update(name: params[:name])
-   			flash.now[:alert] = "Your name is updated!"
+   			flash[:notice] = "Your name is updated!"
    			@same_email_other_flight_b = 	@same_email_p.bookings.create({ flight_id: @selected_flight_id, passenger_n: @passenger_n})
    			if @same_email_other_flight_b.save
    				@pre_avail = Flight.find(@selected_flight_id).avail
    				Flight.update(@selected_flight_id, avail: @pre_avail- @passenger_n)
 	 	    		redirect_to passenger_path(@same_email_p.id)
    			else
-   				flash.now[:alert] = "Failed on saving your new booking!"
+   				flash[:notice] = "Failed on saving your new booking!"
    				render 'new'
    			end  		
    		else
@@ -40,11 +40,11 @@ class PassengersController < ApplicationController
    			Flight.update(@selected_flight_id,
    			 avail: @pre_avail + @same_email_same_flight_b.passenger_n - @passenger_n)
    			@same_email_same_flight_b.update(passenger_n: @passenger_n)   			
-   			flash.now[:alert] = "Your name and your previous booking of this flight are updated!"
+   			flash[:notice] = "Your name and your previous booking of this flight are updated!"
    			if @same_email_p.update(name: params[:name]) && @same_email_same_flight_b.update(passenger_n: @passenger_n)
    				redirect_to passenger_path(@same_email_p.id)   			
    			else
-   				flash.now[:alert] = "Failed on updating your name and your previous booking of this flight!"
+   				flash[:notice] = "Failed on updating your name and your previous booking of this flight!"
    				render 'new'
    			end
    		end	
@@ -54,7 +54,7 @@ class PassengersController < ApplicationController
 	def show
 		@passenger = Passenger.find(params[:id])
 		
-		#flash.now[:alert] = ""
+		#flash[:notice] = ""
 	end
 	  
 	def create
